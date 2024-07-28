3d9954c8f05e1505551309f50bfabfda357c265a
Surowizator <46932793+Surowizator@users.noreply.github.com>
2024-07-28 16:53:37 +0200

    Transparent background

--- lua/config/colorscheme.lua
+++ lua/config/colorscheme.lua
@@ -1,7 +1,14 @@
 local colorscheme = "tokyonight-night"
 
+---@diagnostic disable-next-line
 local status, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 
 if not status then
     vim.notify("Couldn't load the colorscheme.")
+    return
 end
+
+vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalSB", { bg = "none" })
