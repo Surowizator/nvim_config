43b8286bee6edc9b14469ef61fd1133a41dd9bec
Surowizator <46932793+Surowizator@users.noreply.github.com>
2024-08-09 01:48:06 +0200

    Custom compile command for LaTeX

--- lua/config/vimtex.lua
+++ lua/config/vimtex.lua
@@ -4,3 +4,5 @@ vim.g.quickfix_mode = 0
 vim.o.conceallevel = 1
 vim.g.tex_conceal = "abdmg"
 vim.g.vimtex_view_forward_search_on_start = false
+vim.g.vimtex_compiler_method = "generic"
+vim.g.vimtex_compiler_generic = {command = "latexcmp"}
