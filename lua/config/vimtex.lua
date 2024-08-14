6f0c10d71f59f3e97de4eb83cae58bf101db655c
Surowizator <46932793+Surowizator@users.noreply.github.com>
2024-08-14 14:45:32 +0200

    Changed vimtex compiler to pdftex

--- lua/config/vimtex.lua
+++ lua/config/vimtex.lua
@@ -5,4 +5,4 @@ vim.o.conceallevel = 1
 vim.g.tex_conceal = "abdmg"
 vim.g.vimtex_view_forward_search_on_start = false
 vim.g.vimtex_compiler_method = "generic"
-vim.g.vimtex_compiler_generic = {command = "latexcmp"}
+vim.g.vimtex_compiler_generic = { command="pdflatex" }
