8048a792890ae65d880f354ac5e39c264c4e7d22
Surowizator <46932793+Surowizator@users.noreply.github.com>
2024-08-14 21:22:11 +0200

    Reverted back to latexmk, this timeith -pdf flag

--- lua/config/vimtex.lua
+++ lua/config/vimtex.lua
@@ -4,5 +4,11 @@ vim.g.quickfix_mode = 0
 vim.o.conceallevel = 1
 vim.g.tex_conceal = "abdmg"
 vim.g.vimtex_view_forward_search_on_start = false
-vim.g.vimtex_compiler_method = "generic"
-vim.g.vimtex_compiler_generic = { command="pdflatex" }
+vim.g.vimtex_compiler_method = "latexmk"
+vim.g.vimtex_compiler_latexmk = { options = {
+    "-verbose",
+    "-file-line-error",
+    "-synctex=1",
+    "-interaction=nonstopmode",
+    "-pdf",
+} }
