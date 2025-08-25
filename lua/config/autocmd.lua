e1ca2b60845469430a9c04017464fd45966a090c
Maciej Mikołajczak <46932793+Surowizator@users.noreply.github.com>
2025-08-26 01:36:23 +0200

    Removed Treesitter highlighting from unwanted filetypes

--- lua/config/autocmd.lua
+++ lua/config/autocmd.lua
@@ -2,8 +2,14 @@
 vim.api.nvim_create_autocmd('FileType', {
     pattern = "*",
     callback = function()
-        if vim.bo.filetype ~= "tex" then -- for tex VimTex should work
-            vim.treesitter.start()
+        local ft = vim.bo.filetype
+        -- disable for special filetypes and TeX (VimTex takes over)
+        if ft=="NvimTree" or
+            ft=="TelescopePrompt" or ft=="TelescopeResults" or
+            ft=="undotree" or
+            ft=="tex" then
+            return
         end
+        vim.treesitter.start()
     end,
 })
