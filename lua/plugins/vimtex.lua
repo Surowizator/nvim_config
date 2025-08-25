3c74be9189bd343f53f930905b3ba3051bd4bdd0
Maciej Mikołajczak <46932793+Surowizator@users.noreply.github.com>
2025-08-26 00:56:46 +0200

    Removed overriding option config for tex

--- lua/plugins/vimtex.lua
+++ lua/plugins/vimtex.lua
@@ -1,7 +1,6 @@
 vim.g.vimtex_view_method = "zathura"
 vim.g.tex_flavor = "latex"
 vim.g.quickfix_mode = 0
-vim.o.conceallevel = 1
 vim.g.tex_conceal = "abdmg"
 vim.g.vimtex_view_forward_search_on_start = false
 vim.g.vimtex_compiler_method = "latexmk"
