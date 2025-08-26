eac628ebe8a0d9e9e0bab668d815368e3a82c779
Maciej Mikołajczak <46932793+Surowizator@users.noreply.github.com>
2025-08-26 22:23:31 +0200

    Moved plugins to `spec` in lazy config

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -8,107 +8,113 @@ vim.opt.rtp:prepend(lazypath)
 
 -- Setup lazy.nvim
 require("lazy").setup({
-    "nvim-lua/popup.nvim", -- An implementation of the Popup API from vim in Neovim
-    "nvim-lua/plenary.nvim", -- Useful lua functions used ny lots of plugins
-    {
-        "folke/tokyonight.nvim", -- Colorscheme
-        lazy = false,
-        priority = 1000,
-    },
-    {
-        "nvim-telescope/telescope.nvim", -- Fuzzy finder
-        cmd = "Telescope",
-        opts = require("plugins.telescope")
-    },
-    {
-        "nvim-treesitter/nvim-treesitter", -- Language parsers
-        branch = "main",
-        lazy = false,
-        build = ":TSUpdate",
-    },
-    "HiPhish/rainbow-delimiters.nvim", -- Colourful brackets
-    {
-        "windwp/nvim-autopairs", -- Autopairs, integrates with both cmp and treesitter
-        event = "InsertEnter",
-        config = require("plugins.autopairs"),
-    },
-    {
-        "numToStr/Comment.nvim", -- Easily comment stuff
-        event = {"BufNewFile", "BufReadPre"},
-        opts = {},
-    },
-    {
-        "lewis6991/gitsigns.nvim", -- Git integration
-        event = {"BufNewFile", "BufReadPre"},
-        opts = require("plugins.gitsigns"),
-    },
-    "nvim-tree/nvim-web-devicons", -- Nice icons
-    {
-        "nvim-tree/nvim-tree.lua", -- Directory tree
-        cmd = "NvimTreeToggle",
-        opts = require("plugins.nvimtree"),
-    },
-    {
-        "ThePrimeagen/harpoon", -- Easy jumping through files
-        branch = "harpoon2",
-        event = {"BufNewFile", "BufReadPre"}, -- Harpoon loads with keymaps anyway
-        config = require("plugins.harpoon"),
-    },
-    {
-        "hrsh7th/nvim-cmp", -- The completion plugin
-        dependencies = {
-            "hrsh7th/cmp-buffer", -- buffer completions
-            "hrsh7th/cmp-path", -- path completions
-            "hrsh7th/cmp-cmdline", -- cmdline completions
-            -- Some bridges
-            'quangnguyen30192/cmp-nvim-ultisnips',
-            "hrsh7th/cmp-nvim-lsp",
-            "hrsh7th/cmp-nvim-lua",
+    spec = {
+        "nvim-lua/popup.nvim", -- An implementation of the Popup API from vim in Neovim
+        "nvim-lua/plenary.nvim", -- Useful lua functions used ny lots of plugins
+        {
+            "folke/tokyonight.nvim", -- Colorscheme
+            lazy = false,
+            priority = 1000,
         },
-        event = "InsertEnter",
-        config = require("plugins.cmp"),
-    },
-    {
-        'SirVer/ultisnips', -- Snippets engine
-        event = "InsertEnter",
-        init = require("plugins.ultisnips"),
-    },
-    {
-        "williamboman/mason.nvim", -- simple to use language server installer
-        cmd = "Mason",
-        opts = require("plugins.mason"),
-    },
-    {
-        "williamboman/mason-lspconfig.nvim", -- LSP utilising Mason
-        dependencies = {
-            "williamboman/mason.nvim",
-            "neovim/nvim-lspconfig", -- enable LSP
+        {
+            "nvim-telescope/telescope.nvim", -- Fuzzy finder
+            cmd = "Telescope",
+            opts = require("plugins.telescope")
         },
-        event = {"BufNewFile", "BufReadPre"},
-        config = require("plugins.lsp"),
+        {
+            "nvim-treesitter/nvim-treesitter", -- Language parsers
+            branch = "main",
+            lazy = false,
+            build = ":TSUpdate",
+        },
+        {
+            "HiPhish/rainbow-delimiters.nvim", -- Colourful brackets
+            event = {"BufNewFile", "BufReadPre"},
+        },
+        {
+            "windwp/nvim-autopairs", -- Autopairs, integrates with both cmp and treesitter
+            event = "InsertEnter",
+            config = require("plugins.autopairs"),
+        },
+        {
+            "numToStr/Comment.nvim", -- Easily comment stuff
+            event = {"BufNewFile", "BufReadPre"},
+            opts = {},
+        },
+        {
+            "lewis6991/gitsigns.nvim", -- Git integration
+            event = {"BufNewFile", "BufReadPre"},
+            opts = require("plugins.gitsigns"),
+        },
+        "nvim-tree/nvim-web-devicons", -- Nice icons, do not load until something needs it
+        {
+            "nvim-tree/nvim-tree.lua", -- Directory tree
+            cmd = "NvimTreeToggle",
+            opts = require("plugins.nvimtree"),
+        },
+        {
+            "ThePrimeagen/harpoon", -- Easy jumping through files
+            branch = "harpoon2",
+            event = {"BufNewFile", "BufReadPre"}, -- Harpoon loads with keymaps anyway
+            config = require("plugins.harpoon"),
+        },
+        {
+            "hrsh7th/nvim-cmp", -- The completion plugin
+            dependencies = {
+                "hrsh7th/cmp-buffer", -- buffer completions
+                "hrsh7th/cmp-path", -- path completions
+                "hrsh7th/cmp-cmdline", -- cmdline completions
+                -- Some bridges
+                'quangnguyen30192/cmp-nvim-ultisnips',
+                "hrsh7th/cmp-nvim-lsp",
+                "hrsh7th/cmp-nvim-lua",
+            },
+            event = "InsertEnter",
+            config = require("plugins.cmp"),
+        },
+        {
+            'SirVer/ultisnips', -- Snippets engine
+            event = "InsertEnter",
+            init = require("plugins.ultisnips"),
+        },
+        {
+            "williamboman/mason.nvim", -- simple to use language server installer
+            cmd = "Mason",
+            opts = require("plugins.mason"),
+        },
+        {
+            "williamboman/mason-lspconfig.nvim", -- LSP utilising Mason
+            dependencies = {
+                "williamboman/mason.nvim",
+                "neovim/nvim-lspconfig", -- enable LSP
+            },
+            event = {"BufNewFile", "BufReadPre"},
+            config = require("plugins.lsp"),
+        },
+        {
+            "lervag/vimtex", -- TeX plugin
+            ft = "tex",
+            init = require("plugins.vimtex"),
+        },
+        {
+            "mbbill/undotree", -- Tree of changes
+            cmd = "UndotreeToggle",
+        },
+        {
+            "dstein64/vim-startuptime", -- Check startup time
+            cmd = "StartupTime",
+        },
+        {
+            "numToStr/FTerm.nvim", -- Floating terminal, keymap calls require
+            opts = require("plugins.fterm"),
+        },
     },
-    {
-        "lervag/vimtex", -- TeX plugin
-        ft = "tex",
-        init = require("plugins.vimtex"),
-    },
-    {
-        "mbbill/undotree", -- Tree of changes
-        cmd = "UndotreeToggle",
-    },
-    {
-        "dstein64/vim-startuptime", -- Check startup time
-        cmd = "StartupTime",
-    },
-    {
-        "numToStr/FTerm.nvim", -- Floating terminal
-        lazy = true, -- keymap calls require
-        opts = require("plugins.fterm"),
-    },
-
     -- Configure any other settings here. See the documentation for more details.
     -- colorscheme that will be used when installing plugins.
     install = { colorscheme = { "habamax" } },
     -- automatically check for plugin updates
     checker = { enabled = true },
+    defaults = {
+        lazy = true,
+    }
 })
