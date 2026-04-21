be10cf07343a177e301e4d92eda67bcbfd94dfa1
Maciej Mikołajczak <46932793+Surowizator@users.noreply.github.com>
2026-04-21 18:19:00 +0200

    Mathcal snippet

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -8,6 +8,7 @@ return function ()
     vim.keymap.set("n", "<leader>2", function() harpoon:list():select(2) end)
     vim.keymap.set("n", "<leader>3", function() harpoon:list():select(3) end)
     vim.keymap.set("n", "<leader>4", function() harpoon:list():select(4) end)
+    vim.keymap.set("n", "<leader>5", function() harpoon:list():select(5) end)
     vim.keymap.set("n", "<leader>j", function() harpoon:list():prev() end)
     vim.keymap.set("n", "<leader>k", function() harpoon:list():next() end)
 end
